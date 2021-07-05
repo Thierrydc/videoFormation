a5308b7f6ed2449351970a38cd5cb0113ac915e4
Thierrydc <dthierry.talis.bs@gmail.com>
2021-07-05 11:48:04 +0200

    symfony console make:admin:crud for Entity User

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractDashboardController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="app_admin_index")
      */
     public function index(): Response
     {
