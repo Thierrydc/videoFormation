919a1e42b132f494a4c658feab69cdee9a508f6c
Thierrydc <dthierry.talis.bs@gmail.com>
2021-07-06 16:14:25 +0200

    app_main_home

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,21 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\FormationRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AppController extends AbstractController
 {
     /**
      * @Route("/", name="app_main_home")
      */
-    public function index(): Response
+    public function index(FormationRepository $formationRepo, Request $request ): Response
     {
         $user = $this->getUser();
+        $formations = $formationRepo->findAll();
 
-        return $this->render('app/index.html.twig', [
+        return $this->render('formation/index.html.twig', [
             'user' => $user,
+            'formations' => $formations,
         ]);
     }
 }
