585369d13c3212788aa0170fc0c5b95a795967fd
Thierrydc <dthierry.talis.bs@gmail.com>
2021-07-08 09:25:41 +0200

    Formation add image

--- src/Form/FormationFormType.php
+++ src/Form/FormationFormType.php
@@ -22,6 +22,7 @@ class FormationFormType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'name'
             ])
+            ->add('image')
         ;
     }
 
