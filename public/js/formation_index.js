9f524f8b759081dfc4d8c435cd77012dfff55314
Thierrydc <dthierry.talis.bs@gmail.com>
2021-07-21 10:11:26 +0200

    formation_index.js

--- public/js/formation_index.js
+++ public/js/formation_index.js
@@ -1,20 +1,15 @@
 window.onload = () => {
-    const categoryForm = document.querySelector("#category_form");
     const select_category = document.querySelector("#select_category");
 
     select_category.addEventListener("change", e => {
         let category = e.target.value;
-        console.log(category);
 
         // On récupère l'url active
         const Url = new URL(window.location.href);
-
+        
         // On lance la requête ajax
-        fetch(Url.pathname + "?cat=" + category + "&ajax=1", {
-            headers: {
-                "X-Requested-With": "XMLHttpRequest"
-            }
-        }).then(response => 
+        fetch(Url.pathname + "?cat=" + category + "&ajax=1"
+        ).then(response => 
             response.json()
         ).then(data => {
             // On va chercher la zone de contenu
