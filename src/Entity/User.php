1c9ab7a2fa273626564cd4a8a26a2af816aea429
Thierrydc <dthierry.talis.bs@gmail.com>
2021-07-22 13:37:32 +0200

    remove contrainte User->formation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -63,7 +63,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $updatedAt;
 
     /**
-     * @ORM\OneToMany(targetEntity=Formation::class, mappedBy="author")  // fetch="EAGER" =>> AH AH AH ;-)
+     * @ORM\OneToMany(targetEntity=Formation::class, mappedBy="author", orphanRemoval=true)  // fetch="EAGER" =>> AH AH AH ;-)
      */
     private $formations;
 
